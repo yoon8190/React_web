f3c7fe97f100030fcd86adffbfe0fe4905baf474
[yoon8190] <[yoon_8190@naver.com]>
2023-07-26 14:50:28 +0900

    commit

---
+++ reactProject/src/test/java/com/example/reactProject/ReactProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
